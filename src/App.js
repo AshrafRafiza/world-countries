0bb054a591e1553e59f2c08a6933fc482b2a0b8a
Ashraf Rafiza <ashrafrafiza96@gmail.com>
2023-03-29 10:50:27 +0800

    Install Styling libraries
    - install Material UI for icons
    - install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Button>This is button</Button>
     </div>
   );
 }
